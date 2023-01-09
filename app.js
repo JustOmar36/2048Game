59c899c141dfd6a84c1459b0dd651e1e6122d230
Omar Masri <omar@Omars-MacBook-Air.local>
2023-01-09 18:03:52 -0500

    Arrow keys functional

--- app.js
+++ app.js
@@ -14,9 +14,6 @@ document.addEventListener('DOMContentLoaded', () =>  {
         generate()
     }
 
-    createBoard()
-
-
     function generate() {
         let beginningNums = [2, 4]
         var randBeginningNum = Math.floor(Math.random() * beginningNums.length)
@@ -25,5 +22,179 @@ document.addEventListener('DOMContentLoaded', () =>  {
         squares[randomNumber].innerHTML = beginningNums[randBeginningNum]
         // checkForGameOver()
         } else generate()
+    }
+
+    function moveRight(){
+        for (let i=0; i < squares.length; i++){
+            if(i % 4 == 0){
+                let item1 = squares[i].innerHTML
+                let item2 = squares[i+1].innerHTML
+                let item3 = squares[i+2].innerHTML
+                let item4 = squares[i+3].innerHTML
+
+
+                let row = [parseInt(item1), parseInt(item2), parseInt(item3), parseInt(item4)]
+                let l = 0
+
+                while(l != 4){
+                    for (let j=0; j < row.length-1; j++){
+                        if(row[j+1] % 2 != 0){
+                            row[j + 1] = row[j]
+                            row[j] = NaN
+                        }
+                    }
+                    l++
+                }
+
+                for (let i = 0; i < row.length; i++){
+                    if(isNaN(row[i])){
+                        row[i] = null
+                    }
+                }
+
+                squares[i].innerHTML = row[0]
+                squares[i+1].innerHTML = row[1]
+                squares[i+2].innerHTML = row[2]
+                squares[i+3].innerHTML = row[3]
+
+            }
+        }
+        
+    }
+
+    function moveLeft(){
+        for (let i=0; i < squares.length; i++){
+            if(i % 4 == 0){
+  
+                let item1 = squares[i].innerHTML
+                let item2 = squares[i+1].innerHTML
+                let item3 = squares[i+2].innerHTML
+                let item4 = squares[i+3].innerHTML
+
+
+                let row = [parseInt(item1), parseInt(item2), parseInt(item3), parseInt(item4)]
+                let l = 0
+
+                while(l != 4){
+                    for (let j=row.length-1; j > 0; j--){
+                        if(row[j-1] % 2 != 0){
+                            row[j - 1] = row[j]
+                            row[j] = NaN
+                        }
+                    }
+                   l++
+                }
+
+                for (let i = 0; i < row.length; i++){
+                    if(isNaN(row[i])){
+                        row[i] = null
+                    }
+                }
+
+                squares[i].innerHTML = row[0]
+                squares[i+1].innerHTML = row[1]
+                squares[i+2].innerHTML = row[2]
+                squares[i+3].innerHTML = row[3]
+
+            }
+        }
+    }
+
+    function moveUp(){
+        for (let i=0; i < 4; i++){
+                let item1 = squares[i].innerHTML
+                let item2 = squares[i+4].innerHTML
+                let item3 = squares[i+8].innerHTML
+                let item4 = squares[i+12].innerHTML
+
+                let row = [parseInt(item1), parseInt(item2), parseInt(item3), parseInt(item4)]
+                let l = 0
+
+                while(l != 4){
+                    for (let j=row.length-1; j > 0; j--){
+                        if(row[j-1] % 2 != 0){
+                            row[j - 1] = row[j]
+                            row[j] = NaN
+                        }
+                    }
+                    l++
+                }
+
+                for (let i = 0; i < row.length; i++){
+                    if(isNaN(row[i])){
+                        row[i] = null
+                    }
+                }
+
+
+                squares[i].innerHTML = row[0]
+                squares[i+4].innerHTML = row[1]
+                squares[i+8].innerHTML = row[2]
+                squares[i+12].innerHTML = row[3]
+        }
+    }
+    
+    function moveDown(){
+        for (let i=0; i < 4; i++){
+            let item1 = squares[i].innerHTML
+            let item2 = squares[i+4].innerHTML
+            let item3 = squares[i+8].innerHTML
+            let item4 = squares[i+12].innerHTML
+
+            let row = [parseInt(item1), parseInt(item2), parseInt(item3), parseInt(item4)]
+            let l = 0
+
+            while(l != 4){
+                for (let j=0; j < row.length-1; j++){
+                    if(row[j+1] % 2 != 0){
+                        row[j + 1] = row[j]
+                        row[j] = NaN
+                    }
+                }
+                l++
+            }
+
+            for (let i = 0; i < row.length; i++){
+                if(isNaN(row[i])){
+                    row[i] = null
+                }
+            }
+
+
+            squares[i].innerHTML = row[0]
+            squares[i+4].innerHTML = row[1]
+            squares[i+8].innerHTML = row[2]
+            squares[i+12].innerHTML = row[3]
+        }
+    }
+
+    createBoard()
+
+    document.onkeydown = function (event){
+        switch(event.keyCode){
+            case 37:
+                moveLeft()
+                console.log("Left")
+                generate()
+                break
+            
+            case 38:
+                moveUp()
+                console.log("Up")
+                generate()
+                break
+
+            case 39:
+                moveRight()
+                console.log("Right")
+                generate()
+                break
+            
+            case 40:
+                moveDown()
+                console.log("Down")
+                generate()
+                break
+        }
     }
 })
